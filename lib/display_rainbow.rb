3edf372fb1ca0759f22f97a3b8f693c06c700378
David Acosta <david@dsacosta.com>
2018-08-01 05:48:42 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,5 @@
 def display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
   puts "R:[0]"
 end
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 display_rainbow(colors)
